765c2f4d11efd8133a0bcf0f15c5724428fc6f0a
Rashit Khaliullin <rashit.xaliullin@inbox.ru>
2025-03-11 16:50:27 +1000

    Initial commit

---
+++ app/src/main/java/com/example/fitnesstracker/app/App.kt
@@ -0,0 +1,5 @@
+package com.example.fitnesstracker.app
+
+import android.app.Application
+
+class App : Application()
---
+++ app/src/main/java/com/example/fitnesstracker/ui/component/Button.kt
@@ -0,0 +1,28 @@
+package com.example.fitnesstracker.ui.component
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.fitnesstracker.ui.theme.Primary
+
+@Composable
+fun StyledButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        shape = RoundedCornerShape(size = 15.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Primary
+
+        )
+    ) {
+        content()
+    }
+}
