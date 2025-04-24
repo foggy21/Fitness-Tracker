2f26bd234b6b21a62776ef24797bf034dbb1aac8
Rashit Khaliullin <rashit.xaliullin@inbox.ru>
2025-04-24 16:18:45 +1000

    Delete unused import

--- app/src/main/java/com/example/fitnesstracker/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/fitnesstracker/viewmodel/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.fitnesstracker.viewmodel
 
-import androidx.compose.runtime.currentComposer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fitnesstracker.data.database.AuthRepository
